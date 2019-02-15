4ff712d2e3fff28da942c57a6a5a17def1e08524
Crystalwindz <18846188224@163.com>
2019-02-16 01:03:51 +0800

    add asynclog example

---
+++ example/asynclog/asynclog.cpp
@@ -0,0 +1,31 @@
+#include "windz/base/Thread.h"
+#include "windz/log/AsyncLogging.h"
+#include "windz/log/Logger.h"
+
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+using namespace windz;
+using namespace std;
+
+int main(int argc, char **argv) {
+    Logger::SetLevel(Logger::TRACE);
+    AsyncLogging async_logging("async", 10 * 1024 * 1024);
+    async_logging.Start();
+    Logger::SetOutput(
+            [&async_logging](const char *msg, size_t len) { async_logging.Append(msg, len); });
+
+    vector<shared_ptr<Thread>> thread;
+    for (int i = 0; i < 8; i++) {
+        thread.push_back(make_shared<Thread>([] {
+            for (int i = 0; i < 100; i++) {
+                LOG_INFO << i;
+            }
+        }));
+    }
+    for_each(thread.begin(), thread.end(), [](shared_ptr<Thread> &t) { t->Start(); });
+    for_each(thread.begin(), thread.end(), [](shared_ptr<Thread> &t) { t->Join(); });
+    sleep(1);
+    async_logging.Stop();
+}
